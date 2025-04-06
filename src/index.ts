374173a11c46327ad669ec96705c7d7301a81378
wpao <wpaozan@gmail.com>
2025-04-06 22:17:56 +0800

    v1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express, { Request, NextFunction } from "express";
 import cors from "cors";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import { PrismaClient, Prisma } from "@prisma/client";
-
 
 // menerapkan JWT
 import jwt from "jsonwebtoken";
@@ -18,7 +17,7 @@ cron.schedule("0 0 * * *", async () => {
 
 // create prisma client
 // connection database
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 // create express app
 const app = express();
@@ -28,56 +27,54 @@ const app = express();
 app.use(express.json());
 
 // Izinkan semua origin (Tidak disarankan untuk produksi)
-// app.use(cors());
+app.use(cors());
 
-// mengatur izin yang access 
-app.use(cors({
-  origin: [/* "http://localhost:5173" */ "http://futsal-fe:80"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
-
-
+// mengatur izin yang access
+// app.use(cors({
+//   origin: [/* "http://localhost:5173" */ "http://futsal-fe:80"],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true,
+// }));
 
 // // interface
 interface UserData {
-  id: number
-  name: string
-  email: string
+  id: number;
+  name: string;
+  email: string;
 }
 interface ValidationRequest extends Request {
-  userData: UserData
+  userData: UserData;
 }
 
 // validate token setelah di buat pada /login
 const accessValidator = (req: Request, res: any, next: NextFunction) => {
-  const validationReq = req as ValidationRequest
-  const {authorization} = validationReq.headers;
+  const validationReq = req as ValidationRequest;
+  const { authorization } = validationReq.headers;
 
   if (!authorization) {
     return res.status(401).json({
-      message: "Unauthorized"
-    })
+      message: "Unauthorized",
+    });
   }
 
-  const token = authorization.split(" ")[1]
-  const secret = process.env.JWT_SECRET
+  const token = authorization.split(" ")[1];
+  const secret = process.env.JWT_SECRET;
 
   try {
-    const jwtDecoded = jwt.verify(token, secret!)
+    const jwtDecoded = jwt.verify(token, secret!);
     if (typeof jwtDecoded !== "string") {
-      validationReq.userData = jwtDecoded as UserData
+      validationReq.userData = jwtDecoded as UserData;
     }
   } catch (error) {
     return res.status(401).json({
-      message: "Unauthorized"
-    })
+      message: "Unauthorized",
+    });
   }
 
   // lanjutkan ke pemanggilan controller
-  next()
-}
+  next();
+};
 
 // register user
 app.post("/users", async (req: Request, res: any) => {
@@ -118,8 +115,8 @@ app.post("/users", async (req: Request, res: any) => {
 
 // get all users
 app.get("/users", async (_req, res) => {
-  const users = await prisma.user.findMany()
-  res.send(users)
+  const users = await prisma.user.findMany();
+  res.send(users);
 });
 
 // login user & automatically generate token
@@ -140,9 +137,9 @@ app.post("/login", async (req: Request, res: any) => {
     }
 
     // Generate JWT Token
-    const secret = process.env.JWT_SECRET
+    const secret = process.env.JWT_SECRET;
     // const token = jwt.sign({ userId: user.id, username: user.username }, JWT_SECRET, { expiresIn: "1h" });
-    const token = jwt.sign( user, secret!, { expiresIn: "1h" });
+    const token = jwt.sign(user, secret!, { expiresIn: "1h" });
 
     res.status(200).json({ message: "Login successful", data: user, token });
   } catch (error) {
@@ -174,30 +171,34 @@ app.get("/users/:id", async (req: Request, res: any) => {
 });
 
 // delete user
-app.delete("/users/delete/:id", accessValidator, async (req: Request, res: any) => {
-  try {
-    const { id } = req.params;
+app.delete(
+  "/users/delete/:id",
+  accessValidator,
+  async (req: Request, res: any) => {
+    try {
+      const { id } = req.params;
 
-    // Cek apakah user ada
-    const existingUser = await prisma.user.findUnique({ where: { id } });
-    if (!existingUser) {
-      return res.status(404).json({ message: "User not found" });
+      // Cek apakah user ada
+      const existingUser = await prisma.user.findUnique({ where: { id } });
+      if (!existingUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      // Hapus user
+      await prisma.user.delete({ where: { id } });
+
+      res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+      console.error("Error deleting user:", error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
-
-    // Hapus user
-    await prisma.user.delete({ where: { id } });
-
-    res.status(200).json({ message: "User deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting user:", error);
-    res.status(500).json({ message: "Internal Server Error" });
   }
-});
+);
 
 // get all bookings
 app.get("/bookings", async (_req, res) => {
-  const bookings = await prisma.booking.findMany()
-  res.send(bookings)
+  const bookings = await prisma.booking.findMany();
+  res.send(bookings);
 });
 
 // booking
@@ -226,12 +227,14 @@ app.post("/bookings", accessValidator, async (req: Request, res: any) => {
       },
     });
 
-    res.status(201).json({ data: booking, message: "Booking created successfully" });
+    res
+      .status(201)
+      .json({ data: booking, message: "Booking created successfully" });
   } catch (error: any) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
-})
+});
 
 // filter booking date
 // GET /bookings/filter?date=2025-02-19&idUser=1f746f94-0c8e-4360-8b1d-8d70ec62418f
@@ -271,26 +274,32 @@ app.get("/bookings/filter", async (req: Request, res: any) => {
 });
 
 // delete booking
-app.delete("/bookings/delete/:id", accessValidator, async (req: Request, res: any) => {
-  try {
-    const { id } = req.params;
+app.delete(
+  "/bookings/delete/:id",
+  accessValidator,
+  async (req: Request, res: any) => {
+    try {
+      const { id } = req.params;
 
-    // Cek apakah booking ada
-    const existingBooking = await prisma.booking.findUnique({ where: { id } });
+      // Cek apakah booking ada
+      const existingBooking = await prisma.booking.findUnique({
+        where: { id },
+      });
 
-    if (!existingBooking) {
-      return res.status(404).json({ message: "Booking not found" });
+      if (!existingBooking) {
+        return res.status(404).json({ message: "Booking not found" });
+      }
+
+      // Hapus booking
+      await prisma.booking.delete({ where: { id } });
+
+      res.status(200).json({ message: "Booking deleted successfully" });
+    } catch (error) {
+      console.error("Error deleting booking:", error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
-
-    // Hapus booking
-    await prisma.booking.delete({ where: { id } });
-
-    res.status(200).json({ message: "Booking deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting booking:", error);
-    res.status(500).json({ message: "Internal Server Error" });
   }
-});
+);
 
 // edit or add info
 // dua kondisi terbuat karna info tidak boleh mengandung dua info
@@ -299,7 +308,9 @@ app.post("/info", accessValidator, async (req: Request, res: any) => {
     const { content, idLapanganChange } = req.body;
 
     if (!content || !idLapanganChange) {
-      return res.status(400).json({ message: "Content and idLapanganChange are required" });
+      return res
+        .status(400)
+        .json({ message: "Content and idLapanganChange are required" });
     }
 
     // Cek apakah data dengan idUser yang diberikan sudah ada
@@ -326,7 +337,6 @@ app.post("/info", accessValidator, async (req: Request, res: any) => {
     res.status(500).json({ message: "Internal Server Error", error });
   }
 });
-
 
 // get one info
 app.get("/info/:idUser", async (req: Request, res: any) => {
